9da1931270ef1a561a62853e35eecaed2e1f19bb
nicorm <nicorm2000@gmail.com>
2023-04-11 19:47:54 -0300

    Updated MyDllExercises file, created the exercise enum where the exercise can be chosen, created the base variables that i will need, also added customMath library

--- Assets/MyDllExercises.cs
+++ Assets/MyDllExercises.cs
@@ -1,9 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using CustomMath;
 
 public class MyDllExercises : MonoBehaviour
 {
+    public enum Exercise
+    {
+        Uno,
+        Dos,
+        Tres,
+        Cuatro,
+        Cinco,
+        Seis,
+        Siete,
+        Ocho,
+        Nueve,
+        Diez
+    }
+
+    public Exercise exercise = Exercise.Uno;
+    public Vector3 a;
+    public Vector3 b;
+    public Color defaultVectorColor = Color.red;
+
+    private Vec3 vectorA;
+    private Vec3 vectorB;
+    private Vec3 vectorC;
+    private float time = 0;
+    private const int timeLimit = 10;
+
     // Start is called before the first frame update
     void Start()
     {
