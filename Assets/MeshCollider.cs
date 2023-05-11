c680d0bdf68ef46fc4c66c8b2fdbf7bb82174c49
nicorm <nicorm2000@gmail.com>
2023-05-10 23:59:10 -0300

    Modified the update method where i create a plane or "recycle" one if it is already created and the this plane is added to the list of planes

--- Assets/MeshCollider.cs
+++ Assets/MeshCollider.cs
@@ -48,4 +48,48 @@ public class MeshCollider : MonoBehaviour
             planes[i].SetNormalAndPosition(aux, planes[i].normal * planes[i].distance);
         }
     }
+
+    // Summary:
+    // Create an empty plane, if there is an already existing plane in the list it gets equalized
+    // In the loop the actaul vertex are obtained
+    // Afterwards the plane is created with the vertices
+    // The normal is flipped so the plane isn't inverted
+    // The plane is added to the list
+    private void Update()
+    {
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+
+        planes.Clear();
+
+        previousVertex = new List<Vec3>();
+
+        for (int i = 0; i < mesh.GetIndices(0).Length; i+= 3)
+        {
+            Vec3 vertexA = new Vec3(transform.TransformPoint(mesh.vertices[mesh.GetIndices(0)[i]]));
+            Vec3 vertexB = new Vec3(transform.TransformPoint(mesh.vertices[mesh.GetIndices(0)[i + 1]]));
+            Vec3 vertexC = new Vec3(transform.TransformPoint(mesh.vertices[mesh.GetIndices(0)[i + 2]]));
+
+            var plane = new PlaneCustom(vertexA, vertexB, vertexC);
+            plane.normal *= -1;
+            planes.Add(plane);
+        }
+
+        for (int i = 0; i < planes.Count; i++)
+        {
+            planes[i].Flip();
+        }
+
+
+
+
+
+        Debug.Log("Colliding Points: " + pointsInsideMesh.Count + ", " + gameObject);
+    }
+
+
+
+    private void OnDestroy()
+    {
+        isMeshActive = false;
+    }
 }
