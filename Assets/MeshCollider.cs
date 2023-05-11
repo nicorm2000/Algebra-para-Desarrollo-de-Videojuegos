5297038f55b21ada9999699b349f27d7477c09aa
nicorm <nicorm2000@gmail.com>
2023-05-11 09:54:21 -0300

    Created NearestPoint that lets me get the nearestPoint by checking the x y and z values, and the I created the ValueNearestPosition that lets me check the positiion of this points

--- Assets/MeshCollider.cs
+++ Assets/MeshCollider.cs
@@ -8,7 +8,6 @@ public class MeshCollider : MonoBehaviour
     private List<PlaneCustom> planes;
     public bool isMeshActive;
     public List<Vec3> pointsInsideMesh;
-    public List<Vec3> previousVertex;
     public List<Vec3> pointsToCheck;
     public Vec3 nearestPoint;
 
@@ -30,10 +29,9 @@ public class MeshCollider : MonoBehaviour
 
         planes = new List<PlaneCustom>();
         pointsInsideMesh = new List<Vec3>();
-        previousVertex = new List<Vec3>();
         pointsToCheck = new List<Vec3>();
 
-        for (int i = 0; i < mesh.GetIndices(0).Length; i+= 3)
+        for (int i = 0; i < mesh.GetIndices(0).Length; i += 3)
         {
             Vec3 auxA = new Vec3(mesh.vertices[mesh.GetIndices(0)[i]]);
             Vec3 auxB = new Vec3(mesh.vertices[mesh.GetIndices(0)[i + 1]]);
@@ -55,15 +53,14 @@ public class MeshCollider : MonoBehaviour
     // Afterwards the plane is created with the vertices
     // The normal is flipped so the plane isn't inverted
     // The plane is added to the list
+    // Then the nearest point to the plane
     private void Update()
     {
         Mesh mesh = GetComponent<MeshFilter>().mesh;
 
         planes.Clear();
 
-        previousVertex = new List<Vec3>();
-
-        for (int i = 0; i < mesh.GetIndices(0).Length; i+= 3)
+        for (int i = 0; i < mesh.GetIndices(0).Length; i += 3)
         {
             Vec3 vertexA = new Vec3(transform.TransformPoint(mesh.vertices[mesh.GetIndices(0)[i]]));
             Vec3 vertexB = new Vec3(transform.TransformPoint(mesh.vertices[mesh.GetIndices(0)[i + 1]]));
@@ -79,14 +76,52 @@ public class MeshCollider : MonoBehaviour
             planes[i].Flip();
         }
 
-
-
-
+        NearestPoint();
 
         Debug.Log("Colliding Points: " + pointsInsideMesh.Count + ", " + gameObject);
     }
 
+    // Summary:
+    // Gets the nearest point by checking x, y and z points and then assigns them to the nearestPoint Vec3
+    public void NearestPoint()
+    {
+        var nearPoint = nearestPoint;
 
+        var x = ValueNearestPosition(transform.position.x);
+        var y = ValueNearestPosition(transform.position.y);
+        var z = ValueNearestPosition(transform.position.z);
+
+        nearestPoint = Grid.grid[x, y, z];
+    }
+
+    // Summary:
+    // Gets the nearest position value
+    // The position is divided by the grids delta
+    // Then it is checked if the value minus the absolute value of he point (so we get a value between 0 and 0.99), if it is less than half we add 1, if not we return that value
+    // After it is checkd if the value is clamped between 0 and the grid size minus 1
+    // As how clamp works if the value is between those values it returns the value, if not it returns the maximum or minimum
+    // The value is returned as an int
+    private int ValueNearestPosition(float position)
+    {
+        float value;
+
+        var aux = position / Grid.Delta;
+        
+        if (aux - (int)aux > 0.5f)
+        {
+            value = aux + 1.0f;
+        }
+        else
+        {
+            value = aux;
+        }
+
+        value = Mathf.Clamp(value, 0, Grid.Size - 1);
+
+        return (int)value;
+    }
+
+    
 
     private void OnDestroy()
     {
